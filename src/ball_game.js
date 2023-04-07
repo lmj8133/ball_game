1d48b4c2acdb84f45d5af2670d7035746aa07e41
mj.li <mj.li@algoltek.com>
2023-04-07 11:07:07 +0800

    :sparkles: Table Hockey Written by ChatGPT

--- src/ball_game.js
+++ src/ball_game.js
@@ -1,220 +1,162 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-var ballRadius = 10;
-var x = canvas.width / 2;
-var y = canvas.height - 30;
-var dx = 2;
-var dy = -2;
-var last_dx = dx;
-var last_dy = dy;
-var step = 5;
-var speed_count = 0;
-var rebound = false;
-var player1 = {
-    id: 1,
-    height: 10,
-    width: 100,
-    x: canvas.width / 3,
-    y: 0,
-    color: "#FF0000"	// red
-};
-var player2 = {
-    id: 2,
-    height: 100,
-    width: 10,
-    x: 0,
-    y: canvas.height / 3,
-    color: "#000000"	// black
-};
-var player3 = {
-    id: 3,
-    height: 10,
-    width: 100,
-    x: canvas.width / 3,
-    y: canvas.height - 10,
-    color: "#FFA500"	// orange
-};
-var player4 = {
-    id: 4,
-    height: 100,
-    width: 10,
-    x: canvas.width - 10,
-    y: canvas.height / 3,
-    color: "#008000"	// green
-};
+// Get the canvas element
+const canvas = document.getElementById("myCanvas");
+const context = canvas.getContext("2d");
 
-function renderPlayer(object)
-{
-    ctx.fillStyle = object.color;
-    ctx.fillRect(object.x, object.y, object.width, object.height);
-    ctx.strokeRect(object.x, object.y, object.width, object.height);
+// Set up the game board
+const boardWidth = 400;
+const boardHeight = 400;
+
+// Set up the paddles
+const paddleWidth = 10;
+const paddleHeight = 50;
+const paddleSpeed = 5;
+let player1PaddleY = boardHeight / 2 - paddleHeight / 2;
+let player2PaddleY = boardHeight / 2 - paddleHeight / 2;
+
+// Set up the pucks
+const puckRadius = 10;
+const puckSpeed = 5;
+let puckX = boardWidth / 2;
+let puckY = boardHeight / 2;
+let puckDx = puckSpeed;
+let puckDy = puckSpeed;
+
+// Set up the player scores
+let player1Score = 0;
+let player2Score = 0;
+
+var keyCode;
+
+// Draw the game board, paddles, and puck
+function draw() {
+  // Clear the canvas
+  context.clearRect(0, 0, boardWidth, boardHeight);
+
+  // Draw the game board
+  context.beginPath();
+  context.rect(0, 0, boardWidth, boardHeight);
+  context.fillStyle = "#ffffff";
+  context.fill();
+  context.closePath();
+
+  // Draw the paddles
+  context.beginPath();
+  context.rect(0, player1PaddleY, paddleWidth, paddleHeight);
+  context.fillStyle = "#000000";
+  context.fill();
+  context.closePath();
+
+  context.beginPath();
+  context.rect(boardWidth - paddleWidth, player2PaddleY, paddleWidth, paddleHeight);
+  context.fillStyle = "#000000";
+  context.fill();
+  context.closePath();
+
+  // Draw the puck
+  context.beginPath();
+  context.arc(puckX, puckY, puckRadius, 0, Math.PI * 2);
+  context.fillStyle = "#000000";
+  context.fill();
+  context.closePath();
+
+  // Draw the player scores
+  context.font = "30px Arial";
+  context.fillStyle = "#000000";
+  context.fillText(player1Score, 100, 50);
+  context.fillText(player2Score, boardWidth - 100, 50);
 }
 
-function drawBall()
-{
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+// Move the paddles based on user input
+function movePaddles() {
+  document.addEventListener('keypress', function(event) {
+    if (keyCode  == 'KeyQ') {
+      // Move player 1 paddle up
+      if (player1PaddleY > 0) {
+        player1PaddleY -= paddleSpeed;
+        keyCode = null;
+        //alert(`${paddleSpeed}`);
+      }
+    } else if (keyCode  == 'KeyA') {
+      // Move player 1 paddle down
+      if (player1PaddleY < boardHeight - paddleHeight) {
+        player1PaddleY += paddleSpeed;
+        keyCode = null;
+        //alert(`${paddleSpeed}`);
+      }
+    } else if (keyCode  == 'KeyK') {
+      // Move player 2 paddle up
+      if (player2PaddleY > 0) {
+        player2PaddleY -= paddleSpeed;
+        keyCode = null;
+        //alert(`${paddleSpeed}`);
+      }
+    } else if (keyCode  == 'KeyJ') {
+      // Move player 2 paddle down
+      if (player2PaddleY < boardHeight - paddleHeight) {
+        player2PaddleY += paddleSpeed;
+        keyCode = null;
+        //alert(`${paddleSpeed}`);
+      }
+    }
+    keyCode = event.code;
+  },);
 }
 
-function draw()
-{
-    var angle_x = 1;
-    var angle_y = 1;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    renderPlayer(player1);
-    renderPlayer(player2);
-    renderPlayer(player3);
-    renderPlayer(player4);
-
-    /*
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-        dx = -dx;
-    }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-        dy = -dy;
-    }
-    */
-    /*
-    if (((x > canvas.width-ballRadius) && ((y < player4.y + (player4.height)) && (y > player4.y))) ||
-            ((x < ballRadius) && ((y < player2.y + (player2.height)) && (y > player2.y)))) {
-        dx = -dx;
-        speed_count++;
-        //alert(`Keypress: The key pressed is ${speed_count} and the code value is ${speed_count / 10}`);
-        dx *= ((speed_count / 500) + 1);
-    }
-    if (((y > canvas.height-ballRadius) && ((x < player3.x + (player3.width)) && (x > player3.x))) ||
-            ((y < ballRadius) && ((x < player1.x + (player1.width)) && (x > player1.x)))) {
-        dy = -dy;
-        speed_count++;
-        //alert(`Keypress: The key pressed is ${speed_count} and the code value is ${speed_count / 10}`);
-        dy *= ((speed_count / 500) + 1);
-    }
-    */
-    if ((x > canvas.width - ballRadius - 10) && ((y < player4.y + (player4.height)) && (y > player4.y))) {
-        rebound = true;
-        dx = -dx;
-        speed_count++;
-        //alert(`Keypress: The key pressed is ${speed_count} and the code value is ${speed_count / 10}`);
-        dx *= ((speed_count / 200) + 1);
-        angle_x += (Math.abs(y - (player4.y + (player4.height / 2))) / player4.height);
-        if ((y - (player4.y + (player4.height / 2))) > 0) {
-            dy = -dy
-        } else if ((y - (player4.y + (player4.height / 2))) == 0) {
-            dy = 0;
-        }
-    }
-
-    if ((x < ballRadius + 10) && ((y < player2.y + (player2.height)) && (y > player2.y))) {
-        rebound = true;
-        dx = -dx;
-        speed_count++;
-        //alert(`Keypress: The key pressed is ${speed_count} and the code value is ${speed_count / 10}`);
-        dx *= ((speed_count / 200) + 1);
-        angle_x += (Math.abs(y - (player2.y + (player2.height / 2))) / player2.height);
-        if ((y - (player2.y + (player2.height / 2))) > 0) {
-            dy = -dy
-        } else if ((y - (player2.y + (player2.height / 2))) == 0) {
-            dy = 0;
-        }
-    }
-
-    if ((y > canvas.height - ballRadius - 10) && ((x < player3.x + (player3.width)) && (x > player3.x))) {
-        rebound = true;
-        dy = -dy;
-        speed_count++;
-        //alert(`Keypress: The key pressed is ${speed_count} and the code value is ${speed_count / 10}`);
-        dy *= ((speed_count / 200) + 1);
-        angle_y += (Math.abs(x - (player3.x + (player3.width / 2))) / player3.width);
-        if ((x - (player3.x + (player3.height / 2))) < 0) {
-            dx = -dx
-        } else if ((x - (player3.x + (player3.height / 2))) == 0) {
-            dx = 0;
-        }
-    }
-
-    if ((y < ballRadius + 10) && ((x < player1.x + (player1.width)) && (x > player1.x))) {
-        rebound = true;
-        dy = -dy;
-        speed_count++;
-        //alert(`Keypress: The key pressed is ${speed_count} and the code value is ${speed_count / 10}`);
-        dy *= ((speed_count / 200) + 1);
-        angle_y += (Math.abs(x - (player1.x + (player1.width / 2))) / player1.width);
-        if ((x - (player1.x + (player1.height / 2))) < 0) {
-            dx = -dx
-        } else if ((x - (player1.x + (player1.height / 2))) == 0) {
-            dx = 0;
-        }
-    }
-
-    if (rebound == true) {
-        //alert(`x: ${x}, y: ${y}, dx: ${dx}, dy: ${dy}`);
-        if (x < 0) x = 0;
-        if (y < 0) y = 0;
-        if (x > canvas.width) x = canvas.width;
-        if (y > canvas.height) y = canvas.height;
-        rebound = false;
-    }
-
-    if (x < 0 || y < 0 || x > canvas.width || y > canvas.height) {
-        alert(`Game Over`);
-        window.location.reload();
-    }
-
-    x += dx * angle_x;
-    y += dy * angle_y;
-    if (dx == 0) {
-        dx = last_dx;
-    } else {
-        last_dx = dx;
-    }
-
-    if (dy == 0) {
-        dy = last_dy;
-    } else {
-        last_dy = dy;
-    }
+// Move the puck
+function movePuck() {
+  puckX += puckDx;
+  puckY += puckDy;
 }
 
-function remote(obj, key_code, left_up, right_down)
-{
-    // Player 1 & Player 3 move in x direction
-    if (obj.id & 1) {
-        if (key_code == left_up && obj.x != 0) {
-            obj.x -= step;
-        }
-        if (key_code == right_down && obj.x != canvas.width - obj.width) {
-            obj.x += step;
-        }
-    } else {
-    // Player 2 & Player 4 move in y direction
-        if (key_code == left_up && obj.y != 0) {
-            obj.y -= step;
-        }
-        if (key_code == right_down && obj.y != canvas.height - obj.height) {
-            obj.y += step;
-        }
-    }
+// Detect collisions between the puck and the walls or paddles
+function detectCollisions() {
+  // Check for collisions with top and bottom walls
+  if (puckY < puckRadius || puckY > boardHeight - puckRadius) {
+    puckDy = -puckDy;
+  }
+
+  // Check for collisions with paddles
+  if (puckX < paddleWidth && puckY > player1PaddleY && puckY < player1PaddleY + paddleHeight) {
+    puckDx = -puckDx;
+  } else if (puckX > boardWidth - paddleWidth && puckY > player2PaddleY && puckY < player2PaddleY + paddleHeight) {
+    puckDx = -puckDx;
+  }
+
+  // Check for goals
+  if (puckX < puckRadius) {
+    // Player 2 scored
+    player2Score++;
+    resetPuck();
+  } else if (puckX > boardWidth - puckRadius) {
+    // Player 1 scored
+    player1Score++;
+    resetPuck();
+  }
 }
 
-//document.addEventListener('keypress', (event) =>
-document.addEventListener('keydown', (event) =>
+// Reset the puck to the center of the board
+function resetPuck() {
+  puckX = boardWidth / 2;
+  puckY = boardHeight / 2;
+  puckDx = puckSpeed;
+  puckDy = puckSpeed;
+}
+
+// Update the game state and redraw the game
+function update() {
+  movePaddles();
+  movePuck();
+  detectCollisions();
+  draw();
+}
+
+function debug()
 {
-    var keyName = event.key;
-    var keyCode = event.code;
-    //alert(`Keypress: The key pressed is ${keyName} and the code value is ${keyCode}`);
-    //alert(`Keypress: The key pressed is ${player4.y} and the code value is ${y}`);
-    remote (player1, keyCode, 'KeyJ', 'KeyK');
-    remote (player2, keyCode, 'Digit1', 'KeyQ');
-    remote (player3, keyCode, 'KeyD', 'KeyF');
-    remote (player4, keyCode, 'Equal', 'BracketLeft');
-    if (keyCode == 'KeyM') {
-        alert(`x: ${x}, y: ${y}, dx: ${dx}, dy: ${dy}`);
-    }
+    alert(`${keyCode}`);
+}
 
-}, false);
 
-setInterval(draw, 10);
+// Set up the game loop
+setInterval(update, 20);
+setInterval(debug, 1000);
+
